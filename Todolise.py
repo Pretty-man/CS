77aec334e0b9c266a89ae320610db76167273b41
Mylover125 <52639959+Mylover125@users.noreply.github.com>
2019-07-13 21:22:34 +0800

    Add files via upload

--- Todolise.py
+++ Todolise.py
@@ -1,81 +1,107 @@
 import wx
 
-class Todolist(wx.Frame):
+class ToDoList(wx.Frame):
 	"""docstring for todolist"""
 	def __init__(self, parent,title):
-		super(Todolist, self).__init__(parent,title=title,\
-			size=(800,522))
+		super(ToDoList, self).__init__(parent,title=title,\
+			size=(644,522))
 		self.Center()
-
 		#创建控件和布局的承载页面
 		self.panel=wx.Panel(self)
-
 		#创建整个布局
-		vbox=wx.BoxSizer(wx.HORIZONTAL)
+		self.vbox=wx.BoxSizer(wx.HORIZONTAL)
 
 		#第一个模块
 		#垂直添加第一个模块
 		vbox1=wx.BoxSizer(wx.VERTICAL)
 		vbox2 = wx.BoxSizer(wx.VERTICAL)
-		#创建列表与LiseBox类
-		self.list=['my day','important','plan','taxt']
-		self.lb2=wx.ListBox(self.panel,1,choices=self.list,\
-			pos=(0,0),size=(170,463),style=wx.LB_SINGLE)
-		#对ListBox类添加事件，是右上响应左侧变化
-		self.Bind(wx.EVT_LISTBOX,self.on_listbox,self.lb2)
+		#添加五个button
 		#实现添加选项,对Button类添加事件
-		self.button=wx.Button(self.panel,-1,label='+Add',\
-			pos=(50,463),size=(120,20))
-		self.Bind(wx.EVT_BUTTON,self.on_creat,self.button)
+		liststr=["my day","important","plan","taxt"]
+		number = 0
+		self.button_list = [ None for i in range(len(liststr))]
+		for strstr in liststr:
+			self.button_list[number] = wx.Button(self.panel,-1,\
+				label=strstr,pos=(0,number*40),size=(170,40))
+			number+=1
+		self.button_add=wx.Button(self.panel,-1,label='+Add',\
+			pos=(0,443),size=(170,40))
+		self.Bind(wx.EVT_BUTTON,self.on_creat,self.button_add)
+		#self.Bind(wx.EVT_BUTTON,self.on_lift_title,self.button_add)
 		#将控件添加到左侧布局内
-		vbox1.Add(self.lb2,2,wx.EXPAND | wx.ALL,0)
-		vbox1.Add(self.button,2,wx.EXPAND | wx.ALL,0)
+		for num in range(len(liststr)) :
+			vbox1.Add(self.button_list[num],2,wx.EXPAND | wx.ALL,0)
+		vbox1.Add(self.button_add,2,wx.EXPAND | wx.ALL,0)
 		#添加左侧布局到整个布局上
-		vbox.Add(vbox1,1,flag=wx.EXPAND|wx.LEFT)
+		self.vbox.Add(vbox1,1,flag=wx.EXPAND|wx.LEFT)
 
 		#第二个模块
 		vbox2 = wx.BoxSizer(wx.VERTICAL)
-
 		#上半部分
 		#textctrl1 = wx.TextCtrl(panel,pos=(171,0)\
 			#,size=(630,100),style=wx.TE_MULTILINE)
-		self.todo_title = wx.StaticText(self.panel,label='title',pos=(171,0)\
-			,size = (630,100))
+		self.todo_title = wx.StaticText(self.panel,pos=(171,0),\
+			label='title',size = (474,100))
 		vbox2.Add(self.todo_title,1,flag=wx.EXPAND)
-
 		#下半部分
 		#textctrl2 = wx.TextCtrl(self.panel,pos=(191,101)\
 		#	,size=(170,-1),style=wx.TE_MULTILINE)
-		self.list_right_real=['1','2','3','4']
-		self.lb3=wx.ListBox(self.panel,1,choices=self.list_right_real,\
-			pos=(171,101),size=(630,424))
-		vbox2.Add(self.lb3,1,flag=wx.EXPAND)
-
-
-		vbox.Add(vbox2,1,border=10)
-
+		liststr_rdown = [None for  i in range(0)]
+		number_rdowm = 0
+		self.radio_list = [ None for i in range(len(liststr_rdown))]
+		for  str_rdown in liststr_rdown:
+			self.radio_list[number_rdowm] = wx.TextCtrl(self.panel,\
+				-1,pos=(171,100),value=str_rdown)
+			number_rdowm+=1
+		for num_rdowm in range(len(liststr_rdown)):
+			vbox2.Add(self.radio_list[num],2,wx.EXPAND | wx.ALL,0)
+		self.button_add_rdowm = wx.Button(self.panel,-1,label="+Add"\
+			,pos=(480,443),size=(170,40))
+		self.Bind(wx.EVT_BUTTON,self.on_creat_rdown,self.button_add_rdowm)
+		#页面覆盖
+		for number_new in range(len(liststr)):
+			self.Bind(wx.EVT_BUTTON,self.on_creat_newfile,self.button_list[number_new])
+		vbox2.Add(self.button_add_rdowm,2,wx.EXPAND | wx.ALL)
+		self.vbox.Add(vbox2,1,flag=wx.EXPAND|wx.LEFT)
 		self.Show()
 
-	def on_listbox(self,event):
-		'''在右上显示左侧选项，一一对应'''
-		#panel=wx.Panel(self)
-		self.todo_title.SetLabelText(event.GetString())
-		#textctrl2 = wx.TextCtrl(self.panel,pos=(171,101)\
-		# 	,size=(630,463),style=wx.TE_MULTILINE)
+	def on_creat_newfile(self,event):
+		#textctrl=wx.TextCtrl(self.panel,pos=(171,100),size=(455,340))
+		vbox3 = wx.BoxSizer(wx.VERTICAL)
+		liststr_rdown = [None for  i in range(0)]
+		number_rdowm = 0
+		self.radio_list = [ None for i in range(len(liststr_rdown))]
+		for  str_rdown in liststr_rdown:
+			self.radio_list[number_rdowm] = wx.TextCtrl(self.panel,\
+				-1,pos=(171,100),label=str_rdown)
+			number_rdowm+=1
+		for num_rdowm in range(len(liststr_rdown)):
+			vbox3.Add(self.radio_list[num],2,wx.EXPAND | wx.ALL,0)
+		self.button_add_rdowm = wx.Button(self.panel,-1,label="+Add"\
+			,pos=(480,443),size=(170,40))
+		vbox3.Add(self.button_add_rdowm,2,wx.EXPAND | wx.ALL)
+		self.Bind(wx.EVT_BUTTON,self.on_creat_rdown,self.button_add_rdowm)
+		self.vbox.Add(vbox3,1,flag=wx.EXPAND|wx.LEFT)
 
-	def on_creat(self,event):
-		'''该方法获得在窗口内输入的值并返回，返回的
-			即为用户输入的字符串'''
-		awindow=wx.TextEntryDialog(None,\
-			"enter the thing you want to do")
-		if awindow.ShowModal()==wx.ID_OK:
-			return_value=awindow.GetValue()
-			self.list.append(return_value)
-			#self.lb2.InsertItem(return_value)
+	#添加左侧选项卡
+	def on_creat(self,enent):
+		enter = wx.TextEntryDialog(self,'enter the plan')
+		if enter.ShowModal() == wx.ID_OK:
+			return_value=enter.GetValue()
+			self.button_list.append(wx.Button(self.panel,-1,\
+				label=return_value,pos=(0,len(self.button_list)*40),size=(170,40)))
 			return return_value
 
+	#添加右侧选项卡
+	def on_creat_rdown(self,event):
+		enter = wx.TextEntryDialog(self,'enter the real thing')
+		if enter.ShowModal() == wx.ID_OK:
+			return_value_rdown=enter.GetValue()
+			self.radio_list.append(wx.TextCtrl(self.panel,\
+				-1,pos=(191,100+len(self.radio_list)*40),value=return_value_rdown))
+			return return_value_rdown
 
 
 app=wx.App()
-todolist=Todolist(None,"todolistnewm")
+todolist=ToDoList(None,"todolistnewm")
 app.MainLoop()
--- todolist.py
+++ todolist.py
@@ -4,14 +4,12 @@ class ToDoList(wx.Frame):
 	"""docstring for todolist"""
 	def __init__(self, parent,title):
 		super(ToDoList, self).__init__(parent,title=title,\
-			size=(800,522))
+			size=(644,522))
 		self.Center()
-
 		#创建控件和布局的承载页面
 		self.panel=wx.Panel(self)
-
 		#创建整个布局
-		vbox=wx.BoxSizer(wx.HORIZONTAL)
+		self.vbox=wx.BoxSizer(wx.HORIZONTAL)
 
 		#第一个模块
 		#垂直添加第一个模块
@@ -23,42 +21,86 @@ class ToDoList(wx.Frame):
 		number = 0
 		self.button_list = [ None for i in range(len(liststr))]
 		for strstr in liststr:
-			self.button_list[number] = wx.Button(self.panel,-1,label=strstr,pos=(0,number*40),size=(170,40))
+			self.button_list[number] = wx.Button(self.panel,-1,\
+				label=strstr,pos=(0,number*40),size=(170,40))
 			number+=1
 		self.button_add=wx.Button(self.panel,-1,label='+Add',\
 			pos=(0,443),size=(170,40))
-		#.Bind(wx.EVT_BUTTON,self.on_creat,self.button)
+		self.Bind(wx.EVT_BUTTON,self.on_creat,self.button_add)
+		#self.Bind(wx.EVT_BUTTON,self.on_lift_title,self.button_add)
 		#将控件添加到左侧布局内
-		#.Add(self.lb2,2,wx.EXPAND | wx.ALL,0)
-
 		for num in range(len(liststr)) :
 			vbox1.Add(self.button_list[num],2,wx.EXPAND | wx.ALL,0)
 		vbox1.Add(self.button_add,2,wx.EXPAND | wx.ALL,0)
 		#添加左侧布局到整个布局上
-		vbox.Add(vbox1,1,flag=wx.EXPAND|wx.LEFT)
+		self.vbox.Add(vbox1,1,flag=wx.EXPAND|wx.LEFT)
 
 		#第二个模块
 		vbox2 = wx.BoxSizer(wx.VERTICAL)
-
 		#上半部分
 		#textctrl1 = wx.TextCtrl(panel,pos=(171,0)\
 			#,size=(630,100),style=wx.TE_MULTILINE)
-		self.todo_title = wx.StaticText(self.panel,pos=(171,0),label='title'\
-			,size = (630,100))
+		self.todo_title = wx.StaticText(self.panel,pos=(171,0),\
+			label='title',size = (474,100))
 		vbox2.Add(self.todo_title,1,flag=wx.EXPAND)
-
 		#下半部分
 		#textctrl2 = wx.TextCtrl(self.panel,pos=(191,101)\
 		#	,size=(170,-1),style=wx.TE_MULTILINE)
-		self.list_right_real=['1','2','3','4']
-		self.lb3=wx.ListBox(self.panel,1,choices=self.list_right_real,\
-			pos=(171,101),size=(630,424))
-		vbox2.Add(self.lb3,1,flag=wx.EXPAND)
-
-
-		vbox.Add(vbox2,1,border=10)
-
+		liststr_rdown = [None for  i in range(0)]
+		number_rdowm = 0
+		self.radio_list = [ None for i in range(len(liststr_rdown))]
+		for  str_rdown in liststr_rdown:
+			self.radio_list[number_rdowm] = wx.CheckBox(self.panel,\
+				-1,pos=(171,100),label=str_rdown)
+			number_rdowm+=1
+		for num_rdowm in range(len(liststr_rdown)):
+			vbox2.Add(self.radio_list[num],2,wx.EXPAND | wx.ALL,0)
+		self.button_add_rdowm = wx.Button(self.panel,-1,label="+Add"\
+			,pos=(480,443),size=(170,40))
+		self.Bind(wx.EVT_BUTTON,self.on_creat_rdown,self.button_add_rdowm)
+		#页面覆盖
+		for number_new in range(len(liststr)):
+			self.Bind(wx.EVT_BUTTON,self.on_creat_newfile,self.button_list[number_new])
+		vbox2.Add(self.button_add_rdowm,2,wx.EXPAND | wx.ALL)
+		self.vbox.Add(vbox2,1,flag=wx.EXPAND|wx.LEFT)
 		self.Show()
+
+	def on_creat_newfile(self,event):
+		#textctrl=wx.TextCtrl(self.panel,pos=(171,100),size=(455,340))
+		vbox3 = wx.BoxSizer(wx.VERTICAL)
+		liststr_rdown = [None for  i in range(0)]
+		number_rdowm = 0
+		self.radio_list = [ None for i in range(len(liststr_rdown))]
+		for  str_rdown in liststr_rdown:
+			self.radio_list[number_rdowm] = wx.CheckBox(self.panel,\
+				-1,pos=(171,100),label=str_rdown)
+			number_rdowm+=1
+		for num_rdowm in range(len(liststr_rdown)):
+			vbox3.Add(self.radio_list[num],2,wx.EXPAND | wx.ALL,0)
+		self.button_add_rdowm = wx.Button(self.panel,-1,label="+Add"\
+			,pos=(480,443),size=(170,40))
+		vbox3.Add(self.button_add_rdowm,2,wx.EXPAND | wx.ALL)
+		self.Bind(wx.EVT_BUTTON,self.on_creat_rdown,self.button_add_rdowm)
+		self.vbox.Add(vbox3,1,flag=wx.EXPAND|wx.LEFT)
+
+	#添加左侧选项卡
+	def on_creat(self,enent):
+		enter = wx.TextEntryDialog(self,'enter the plan')
+		if enter.ShowModal() == wx.ID_OK:
+			return_value=enter.GetValue()
+			self.button_list.append(wx.Button(self.panel,-1,\
+				label=return_value,pos=(0,len(self.button_list)*40),size=(170,40)))
+			return return_value
+
+	#添加右侧选项卡
+	def on_creat_rdown(self,event):
+		enter = wx.TextEntryDialog(self,'enter the real thing')
+		if enter.ShowModal() == wx.ID_OK:
+			return_value_rdown=enter.GetValue()
+			self.radio_list.append(wx.CheckBox(self.panel,\
+				-1,pos=(191,100+len(self.radio_list)*40),label=return_value_rdown))
+			return return_value_rdown
+
 
 app=wx.App()
 todolist=ToDoList(None,"todolistnewm")
